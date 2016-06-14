63bbd3bc487b796113e575b8976d917372b9e972
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-14 11:00:56 -0600

    add index.html.erb

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,8 +5,10 @@ class Store < OpenStruct
   end
 
   def self.search_by_zip(zip)
-    store_results = service.get_stores(zip)["stores"]
-    store_results.map {|store| Store.new(store)}
+    store_results = service.get_stores(zip)
+    stores = store_results["stores"]
+    total = store_results["total"]
+    stores.map {|store| Store.new(store)}
   end
 
 end
